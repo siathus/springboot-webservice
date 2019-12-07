57f93705262fc255dcf48f9ddfc057727ec8b60f
sklim0932 <sklim0932@gmail.com>
2019-12-07 23:07:01 +0900

    게시글 수정 및 삭제 구현 및 관련 뷰 페이지 생성

--- src/main/java/com/siathus/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/siathus/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.siathus.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
     - MyBatis 등에서 DAO로 불리는 DB Layer 접근자.
@@ -9,5 +12,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
     - 주의점 : Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다.
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    /*
+        1. @Query
+            - Spring Data Jpa에서 제공하지 않는 메소드를 쿼리로 작성할 수 있게 해주는 어노테이션.
+     */
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")  // 1
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/siathus/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/siathus/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.siathus.book.springboot.service.posts;
 
 import com.siathus.book.springboot.domain.posts.Posts;
 import com.siathus.book.springboot.domain.posts.PostsRepository;
+import com.siathus.book.springboot.web.dto.PostsListResponseDto;
 import com.siathus.book.springboot.web.dto.PostsResponseDto;
 import com.siathus.book.springboot.web.dto.PostsSaveRequestDto;
 import com.siathus.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +30,28 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    /*
+        1. @Transactional(readOnly = true)
+            - 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
+            - 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하면 좋다.
+        2. .map(PostsListResponseDto::new)
+            - .map(posts -> new PostsListResponseDto(posts))와 같다.
+            - postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostListsResponseDto로 변환하여 List로 반환하는 메소드이다.
+     */
+    @Transactional(readOnly = true) // 1
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // 2
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
