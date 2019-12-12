0558dee52be845fddec511dea82c2d1b15221cd8
sklim0932 <sklim0932@gmail.com>
2019-12-12 21:31:14 +0900

    무중단 배포 스크립트 생성

---
+++ scripts/profile.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# 쉬고 있는 profile 찾기 : real1과 real2
+
+function find_idle_profile()
+{
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+  # 40x, 50x 에러를 모두 포함
+  if [ ${RESPONSE_CODE} -ge 400 ]
+  then
+    CURRENT_PROFILE=real2
+  else
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
+  fi
+
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+  else
+    IDLE_PROFILE=real1
+  fi
+
+  echo "${IDLE_PROFILE}"
+}
