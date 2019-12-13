1b142b44022e338a54b13dc17f3f78d96ef3dccc
sklim0932 <sklim0932@gmail.com>
2019-12-13 18:54:07 +0900

    posts-update.mustache 오타 수정

--- src/main/java/com/siathus/book/springboot/web/PostsApiController.java
+++ src/main/java/com/siathus/book/springboot/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.siathus.book.springboot.web;
 
 import com.siathus.book.springboot.service.posts.PostsService;
+import com.siathus.book.springboot.web.dto.PostsListResponseDto;
 import com.siathus.book.springboot.web.dto.PostsResponseDto;
 import com.siathus.book.springboot.web.dto.PostsSaveRequestDto;
 import com.siathus.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -33,5 +36,10 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
